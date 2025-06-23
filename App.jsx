a8c81ff04d3b488cccea51c02a8ad85d0d820ffe
Archana Sarang <archana.sarang@gmail.com>
2025-06-23 15:13:48 +0530

    add an element into existing list
    preventing adding blank item,
    preventing adding blank item to the list 
    means, it shows an alert when you trying to add a blank item to the list

--- App.jsx
+++ App.jsx
@@ -1,34 +1,63 @@
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react'
 import './App.css'
-const initailList = [
-                      {id : 1, item: 'abc'},
-                      {id : 2, item: 'xyz'},
-                      {id : 3, item: 'pqr'}];
 
+const myList = [
+  { id: 1, item: 'milk' },
+  { id: 2, item: 'veggies' },
+  { id: 3, item: 'eggs' },
+  { id: 4, item: 'fruits' },
+  { id: 5, item: 'flowers for pooja' }];
+
+let nextId = 6;
 function App() {
 
-  const [cart, setCart] = useState(initailList);
-  function handleDelete(cartId){
-    setCart(
-      cart.filter((c)=>c.id !== cartId ),
-      
-    );
+  const [list, setList] = useState(myList);
+  const [item, setItem] = useState('');
 
-  }
-  
+  function handleAddItem() {
+    // if(!item.trim()) // preventing adding blank item
+    //   {
+    //     return;
+    //     // alert('add some item');
+    //   }
+    if(item == ''){
+      alert('add something!');
+      return;
+    }else{
+      const insertIdx = 1;
+
+    const newList = [
+      ...list.slice(0, insertIdx),
+      { id: nextId, item: item },
+      ...list.slice(insertIdx)
+
+    ];
+
+    nextId++;
+    setList(newList);
+    setItem('');
+    }
+    
+
+}
   return (
     <>
-      <h1>Items in a cart</h1>
-      <ul>
-        {cart.map((c)=>(
-          <li key={c.id}>{c.item}{' '}
-          
-          <button onClick={()=>{handleDelete(c.id)}}>Delete it</button>
-          </li>
+      <h1>Adding new item in an exising list</h1>
+
+
+      <input type="text"
+        value={item}
+        placeholder='add item name here'
+        onChange={(e) => setItem(e.target.value)}
+      /> {' '}
+
+      <button onClick={handleAddItem}>Insert</button>
+      <ol>
+        {list.map((itm) => (
+          <li key={itm.id}>{itm.item}</li>
         ))}
-      </ul>
-      
+      </ol>
     </>
   )
 }
